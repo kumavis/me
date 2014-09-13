7fcbb82d0e94dbb22729b9962fce97b8b48d0b95
aaron <a.aa.akio.aaron@gmail.com>
2014-09-13 11:03:42 -0700

    I know kung-foo

---
+++ assets/js/index.js
@@ -0,0 +1,2 @@
+// i know kung-foo
+console.log("%c+", "font-size: 1px; padding: 180px 320px; line-height: 360px; background: url(http://i.imgur.com/kZfvHRV.gif); background-size: 640px 360px; color: transparent;")
