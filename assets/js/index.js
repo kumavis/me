9a09037bd87dab288de1de11cffa9928e9942386
aaron <a.aa.akio.aaron@gmail.com>
2014-09-14 14:03:54 -0700

    Mobile - Tap to flip project card

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,19 @@
 // i know kung-foo
-console.log("%c+", "font-size: 1px; padding: 180px 320px; line-height: 360px; background: url(http://i.imgur.com/kZfvHRV.gif); background-size: 640px 360px; color: transparent;")
+console.log('%c+', 'font-size: 1px; padding: 180px 320px; line-height: 360px; background: url(http://i.imgur.com/kZfvHRV.gif); background-size: 640px 360px; color: transparent;')
+
+var cardElements = [].slice.apply(document.querySelectorAll('.project-card-container'))
+cardElements.forEach(function(cardElement) {
+  cardElement.addEventListener('mouseover', selectCard.bind(null, cardElement), false);
+  cardElement.addEventListener('touchstart', selectCard.bind(null, cardElement), false);
+})
+
+function selectCard(cardElement, event) {
+  deselectAllCards()
+  cardElement.classList.add('selected')
+}
+
+function deselectAllCards() {
+  cardElements.forEach(function(cardElement) {
+    cardElement.classList.remove('selected')
+  })
+}
